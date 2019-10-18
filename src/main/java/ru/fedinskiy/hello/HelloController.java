a69d770b989a479b73ed08281efcec0dcc30d695
Fedor <fdudinskiy@yandex.ru>
2019-10-18 22:52:35 +0300

    +Initial class for outside testing

---
+++ src/main/java/ru/fedinskiy/hello/HelloController.java
@@ -0,0 +1,15 @@
+package ru.fedinskiy.hello;
+
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Produces;
+
+@Controller("/hello")
+public class HelloController {
+	@Get("/")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getHello(){
+		return "Alive!";
+	}
+}
