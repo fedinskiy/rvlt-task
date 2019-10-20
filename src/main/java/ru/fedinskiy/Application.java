db72eed394af871a285496a3d6da0b6eceec8301
Fedor <fdudinskiy@yandex.ru>
2019-10-20 14:51:15 +0300

    ~Refactoring

--- src/main/java/ru/fedinskiy/Application.java
+++ src/main/java/ru/fedinskiy/Application.java
@@ -3,7 +3,7 @@ package ru.fedinskiy;
 import io.micronaut.runtime.Micronaut;
 
 public class Application {
-    public static void main(String[] args) {
-        Micronaut.run(Application.class);
-    }
+	public static void main(String[] args) {
+		Micronaut.run(Application.class);
+	}
 }
