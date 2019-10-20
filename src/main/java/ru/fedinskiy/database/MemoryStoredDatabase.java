ca47afa5cb78f072033f13bdcb72d5bd25417466
Fedor <fdudinskiy@yandex.ru>
2019-10-20 19:02:50 +0300

    -Outdated API

--- src/main/java/ru/fedinskiy/database/MemoryStoredDatabase.java
+++ src/main/java/ru/fedinskiy/database/MemoryStoredDatabase.java
@@ -5,7 +5,6 @@ import io.micronaut.context.annotation.Context;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
-import java.util.stream.IntStream;
 
 @Context
 public class MemoryStoredDatabase implements AccountDatabase {
@@ -13,14 +12,6 @@ public class MemoryStoredDatabase implements AccountDatabase {
 
 	public MemoryStoredDatabase() {
 		this.accounts = new HashMap<>();
-		load(accounts);
-	}
-
-	private static void load(Map<Integer, Account> accounts) {
-		IntStream.rangeClosed(0, 10)
-				.mapToObj(MemoryStoredAccount::new)
-				.peek(account -> account.add(100))
-				.forEach(acc -> accounts.put(acc.getId(), acc));
 	}
 
 	@Override
